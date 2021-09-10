65eec2f9c32d6b1db2a607a892c71c9ed4ee0f96
Yasmeen <Yasmeen.youssef.alsaedi@gmail.com>
2021-09-10 16:32:49 +0300

    Rachael Tatman s meetup has been  added

---
+++ subscription.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+         $to = "pydataJeddah@gmail.com";
+         $subject = "This is subject";
+         
+         $message = "<b>This is HTML message.</b>";
+         $message .= "<h1>This is headline.</h1>";
+         
+         $header = "From:pydataJeddah@gmail.com \r\n";
+  
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         
+         $retval = mail ($to,$subject,$message,$header);
+         
+         if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
+
+
+header("Location: ./"); 
+exit();
+
+
+?>
